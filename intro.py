bfa194a5a0f5e4acf541985b5f03f9db9bef1e58
Matthew <matthewb@skilstak.sh>
2015-09-03 16:27:40 -0400

    save

---
+++ intro.py
@@ -0,0 +1,10 @@
+"""The Return of the Dark Dragon Lord
+
+In the realm of _______ there once was a baby dragon.
+Its name was Maylanth he also had a brother named Syrilth.
+Years and years went by and Syrilth was always treated better
+then Maylanth. Every single year Maylanth grew mader and mader,
+one day when his family was sleeping he snuck away to a cave.
+In the cave he found that there was a purple gem so he took it
+thinking that it was good luck. He fell asleep while he was asleep Maylanth found him.
+Syrilth woke up to find that his brother took his gem and smashed it.
