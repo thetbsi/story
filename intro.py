3b615f1ebccf57719ecb11c421bc4bbaec0f613f
thetbsi <maytt4b567@gmail.com>
2015-09-10 15:56:07 -0400

    save

--- intro.py
+++ intro.py
@@ -3,8 +3,17 @@
 In the realm of _______ there once was a baby dragon.
 Its name was Maylanth he also had a brother named Syrilth.
 Years and years went by and Syrilth was always treated better
-then Maylanth. Every single year Maylanth grew mader and mader,
+then Maylanth. Every single year Maylanth grew worse and worse,
 one day when his family was sleeping he snuck away to a cave.
 In the cave he found that there was a purple gem so he took it
-thinking that it was good luck. He fell asleep while he was asleep Maylanth found him.
-Syrilth woke up to find that his brother took his gem and smashed it.
+thinking that it was good luck. He fell asleep while he was asleep Syrilth found him.
+Maylanth woke up to find that his brother took his gem and smashed it. He grew evil inside.
+A long time passed and Maylanth became the dragon Lord of Evil, and his brother Syrilth became the 
+dragon Lord of Good
+
+
+
+
+
+
+
